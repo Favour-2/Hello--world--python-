f4d79306ef53b5b5d8bc8df88b9d0a04c1941928
Favour-2 <imoniro-favour@calebuniversity.edu.ng>
2024-11-02 22:04:31 +0100

    Hello.py

--- Hello.py
+++ Hello.py
@@ -1,4 +1,11 @@
+echo "# Hello--world--python-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/print('hello word')
+/Hello--world--python-.git
+git push -u origin main
 
-print('hello word')
 
 
